2e10ade06b1c1162aeba9766185da7d5dd2e4ec8
Gagan-B <62099897+Gagan-B@users.noreply.github.com>
2020-03-13 09:12:02 +0530

    Add files via upload

---
+++ Assignment-13/src/com/epsilon/training/programs/Main.java
@@ -0,0 +1,27 @@
+package com.epsilon.training.programs;
+
+public class Main {
+
+	public static String generatePassword(int length) {
+		// do stuff here
+		double rand;
+		int index;
+		String password = "";
+		String[] words = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
+				"S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+				"n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7",
+				"8", "9", "~", "!", "@", "#", "$", "%", "^", "&", "*" };
+		for(int i=0;i<length;i++) {
+			rand = Math.random();
+			index = (int) (rand*71);
+			password = password + words[index];
+		}
+		return password;
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println(generatePassword(6));
+	}
+
+}
